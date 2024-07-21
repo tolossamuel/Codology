f314fdfbf95f1e19435459f19c7a63e1deddf16c
Webi Muleta <133377877+Dirac1235@users.noreply.github.com>
2024-07-21 16:57:14 +0300

    file

--- Frontend/src/components/user_components/SideBar.tsx
+++ Frontend/src/components/user_components/SideBar.tsx
@@ -68,7 +68,7 @@ function SideBar() {
         </li>
         <li>
           <Link
-            href="#"
+            href="/assistance"
             className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-white hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
           >
             <span className="inline-flex justify-center items-center ml-4">
